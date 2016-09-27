0c94e6bd06c24dab5736b8f3d3d2ef88c1f58af0
lenovo <lenovo@ARTH>
2016-09-27 19:44:54 +0530

    OrderPage Initiated

---
+++ OrderPage/OrderPage/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OrderPage.Startup))]
+namespace OrderPage
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
